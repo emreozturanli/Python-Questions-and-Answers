b24624cebeb571ddc6aa519f743a961fa34f19f6
emre <emreozturanli1@gmail.com>
2022-04-09 18:27:17 +0300

    permutations was added

---
+++ permutations.py
@@ -0,0 +1,39 @@
+# ********PERMUTATIONS*******
+
+# leetcode
+
+# Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+# Example 1:
+
+# Input: nums = [1,2,3]
+# Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+
+# Example 2:
+
+# Input: nums = [0,1]
+# Output: [[0,1],[1,0]]
+
+# Example 3:
+
+# Input: nums = [1]
+# Output: [[1]]
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 6
+# -10 <= nums[i] <= 10
+# All the integers of nums are unique.
+
+
+#ANSWER
+
+solution = [[]]
+nums = [1,2,3]
+nums_set = set(nums)
+for index in range(len(nums)):
+
+  solution = [i + [j] for i in solution for j in nums_set.difference(set(i))]
+
+print(solution)
